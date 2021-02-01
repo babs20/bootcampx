393f836efc993de6bb0c4734d73b3e37efd60991
Brady <thevillemc@gmail.com>
2021-02-01 19:06:12 +0000

    add query for crossover students

---
+++ 1_queries/7_rollover_students.sql
@@ -0,0 +1,8 @@
+SELECT students.name AS student_name,
+cohorts.name AS cohort_name,
+cohorts.start_date AS cohort_start_date,
+students.start_date AS studentq_start_date
+FROM students
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.start_date != students.start_date
+ORDER BY cohorts.start_date;
